7d5e20176a5f7eeec0519ba6ee3e903140c9464d
javatechy <deepak@192.168.1.63>
2018-09-05 14:31:25 +0530

    docker enve

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 # dokr
 
 Package for ECS help and docker tasks
+
+
+
+
+```
+pip install virtualenv
+```
+### Create one
+
+```
+virtualenv my_env
+
+source my_env/bin/activate
+```
+### turn off
+
+```
+deactivate
+```
--- setup.py
+++ setup.py
@@ -3,21 +3,24 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-
 setuptools.setup(
-     name='dokr',    
-     version='0.1',      
-     scripts=['dokr'] ,
-     author="Deepak Kumar",
-     author_email="deepak.kumar.iet@gmail.com",
-     description="A Docker and AWS utility package",
-     long_description=long_description,
-     long_description_content_type="text/markdown",
-     url="https://github.com/javatechy/dokr",
-     packages=setuptools.find_packages(),
-     classifiers=[
-         "Programming Language :: Python :: 3",
-         "License :: OSI Approved :: MIT License",
-         "Operating System :: OS Independent",
-     ],
+    name='dokr',
+    version='0.2',
+    scripts=['dokr'] ,
+    author="Deepak Kumar",
+    author_email="deepak.kumar.iet@gmail.com",
+    description="A Docker and AWS utility package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/javatechy/dokr",
+    packages=setuptools.find_packages(),
+    py_modules = ['ecs_helper', 'docker_helper'],
+    install_requires=[
+        'ecs-deploy'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
  )
