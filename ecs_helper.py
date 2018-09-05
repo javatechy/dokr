1996a0f4f7f568ee020c39c8f7c51fc02a80e4e8
javatechy <deepak@192.168.1.63>
2018-09-05 12:49:23 +0530

    Imaged Updation doen

--- ecs_helper.py
+++ ecs_helper.py
@@ -1,54 +1,78 @@
 import sys
 import os
-import utils.helper as utils;
+import utils.helper as utils
+import json
+
 
 def login_ecs():
-    login = utils.cmdExec("aws ecr get-login   | sed  's/-e none//g'")
-    utils.cmdExec(login)
+    login = utils.cmd_exec("aws ecr get-login   | sed  's/-e none//g'")
+    utils.cmd_exec(login)
+
+
+def get_last_task_definations(family_prefix):
+    task_definations = utils.cmd_exec("aws ecs list-task-definitions --family-prefix " + family_prefix);
+    return json.loads(task_definations)['taskDefinitionArns'][-1]
+
+
+def get_task_defination_json(task_def_name):
+    task_defination_json = utils.cmd_exec("aws ecs describe-task-definition --task-definition " + task_def_name);
+    return json.loads(task_defination_json)
+
+
+def update_task_image_version(task_def_json, version_number):
+    imageName = task_def_json['taskDefinition']['containerDefinitions'][0]['image'];
+    imageName = imageName.split(':')[0] + ':' + version_number
+    task_def_json['taskDefinition']['containerDefinitions'][0]['image'] = imageName;
+    print (json.dumps(task_def_json, indent=4))
+    return task_def_json;
 
 def deploy():
-    print ('deploying on ecs cluster')
-    """
-    BUILD_NUMBER="10"
-    REGION="ap-south-1"
-    SERVICE_NAME="docker-ecs-boot-service"
-    CLUSTER="ecs-cluster3"
-    IMAGE_VERSION="v_" + BUILD_NUMBER
-    TASK_FAMILY="docker_ecs_app_image"
-    # Create a new task definition for this build
-    # sed -e "s;%BUILD_NUMBER%;${BUILD_NUMBER};g" ./docker_boot.json > docker_boot_app-v_8.json
-    utils.cmdExec("sed -e 's;%BUILD_NUMBER%;"+BUILD_NUMBER+ ";g ./docker_boot.json > docker_boot_app-v_"+BUILD_NUMBER+".json") 
     
+    build_number = "10"
+    region = "ap-south-1"
+    service_name = "docker-ecs-boot-service"
+    cluster = "ecs-cluster3"
+    image_version = "v_" + build_number
+    task_family = "docker_ecs_app_image"
+    
+    last_task_def_name = get_last_task_definations(task_family)
+    task_def_json = get_task_defination_json(last_task_def_name)
+    task_def_json = update_task_image_version(task_def_json, image_version)
+    
+    print ('Last Updated Task definations : ' + json.dumps(task_def_json, indent=4))
+    
+    # Create a new task definition for this build
     # aws ecs register-task-definition --family docker_ecs_app_image --cli-input-json file://docker_boot_app-v_8.json
-    aws ecs register-task-definition --family $TASK_FAMILY --cli-input-json file://docker_boot_app-v_${BUILD_NUMBER}.json
+    utils.cmd_exec("aws ecs register-task-definition --family " + task_family + " --cli-input-json " + json.dumps(task_def_json) )
 
+    """
+    
     # Update the service with the new task definition and desired count
     # aws ecs describe-task-definition --task-definition docker_ecs_app_image | egrep "revision" | tr "/" " " | awk '{print $2}' | sed 's/"$// | sed 's/,/ /g''
-    REVISION=`aws ecs describe-task-definition --task-definition $TASK_FAMILY | egrep "revision" | tr "/" " " | awk '{print $2}' | sed 's/"$//' | sed 's/,/ /g'`
+    REVISION = utils.cmd_exec("aws ecs describe-task-definition --task-definition $task_family | egrep 'revision' | tr '/' ' ' | awk '{print $2}' | sed 's/\"$//' | sed 's/,/ /g'")
 
-    echo "current revision is $REVISION"
+    print ("current revision is $REVISION")
 
     # aws ecs describe-services --services docker-ecs-boot-service --cluster ecs-cluster3 --region ap-south-1
 
     print "Checking if a service for this task is created or not"
 
-    SERVICES=`aws ecs describe-services --services ${SERVICE_NAME} --cluster ${CLUSTER} --region ${REGION} | jq .failures[]`
+    SERVICES = utils.cmd_exec("aws ecs describe-services --services ${service_name} --cluster ${cluster} --region ${region} | jq .failures[]")
 
-    echo "value of SERVICES  : $SERVICES"
+    print ("value of SERVICES " + SERVICES)
 
-    #Create or update service
+    # Create or update service
 
     if SERVICES == "":
         print "updating older service"
         # aws ecs describe-services --services docker-ecs-boot-service --cluster ecs-cluster3 --region ap-south-1 | jq .services[].desiredCount
-        DESIRED_COUNT=`aws ecs describe-services --services ${SERVICE_NAME} --cluster ${CLUSTER} --region ${REGION} | jq .services[].desiredCount`
-        if [ ${DESIRED_COUNT} = "0" ]; then
-        DESIRED_COUNT="1"
+        DESIRED_COUNT = utils.cmd_exec("aws ecs describe-services --services ${service_name} --cluster ${cluster} --region ${region} | jq .services[].desiredCount")
+        if DESIRED_COUNT == "0":
+            DESIRED_COUNT = "1"
 
-        echo "updating service definations"
-        aws ecs update-service --cluster ${CLUSTER} --region ${REGION} --service ${SERVICE_NAME} --task-definition ${TASK_FAMILY}:${REVISION} --desired-count ${DESIRED_COUNT} --deployment-configuration maximumPercent=100,minimumHealthyPercent=0
-    else
-        echo "entered new service"
-        aws ecs create-service --service-name ${SERVICE_NAME} --desired-count 1 --task-definition ${TASK_FAMILY} --cluster ${CLUSTER} --region ${REGION}
-
+        print "updating service definations"
+        utils.cmd_exec("aws ecs update-service --cluster ${cluster} --region ${region} --service ${service_name} --task-definition ${task_family}:${REVISION} --desired-count ${DESIRED_COUNT} --deployment-configuration maximumPercent=100,minimumHealthyPercent=0")
+    else :
+        print("entered new service")
+        utils.cmd_exec("aws ecs create-service --service-name ${service_name} --desired-count 1 --task-definition ${task_family} --cluster ${cluster} --region ${region}")
     """
